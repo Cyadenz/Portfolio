4eae6e4dd369850b416757080d5dae5fddff18cc
Cyadenz <ugo.perniceni@hotmail.fr>
2018-03-20 00:59:18 +0100

    Adaptation couleurs + progression compétences

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -48,13 +48,13 @@
               <a class="nav-link js-scroll-trigger" href="#about">À propos</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#veille">Veille technologique</a>
+              <a class="nav-link js-scroll-trigger" href="#competences">Compétences</a>
             </li>
             <li class="nav-item">
               <a class="nav-link js-scroll-trigger" href="#Projets">Projets</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#competences">Compétences</a>
+              <a class="nav-link js-scroll-trigger" href="#veille">Veille technologique</a>
             </li>
             <li class="nav-item">
               <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
@@ -88,7 +88,7 @@
             <h2 class="text-center section-heading text-white"><i class="fa fa-info-circle fa-1.5x"></i> À propos</h2>
             <hr class="light my-4">
             <div class="LigneV mb-4"> 
-              <p class="text-faded mb-4 ml-2">Je suis actuellement étudiant en seconde année de BTS SIO (Services Informatiques aux Organisations), spécialisé dans l'option SLAM (Solutions Logicielles et Applications Métiers) et activement à la recherche d'une alternance pour pouvoir continuer mes études en licence profesionnelle !<br /><br />
+              <p class="text-white mb-4 ml-2">Je suis actuellement étudiant en seconde année de BTS SIO (Services Informatiques aux Organisations), spécialisé dans l'option SLAM (Solutions Logicielles et Applications Métiers) et activement à la recherche d'une alternance pour pouvoir continuer mes études en licence profesionnelle !<br /><br />
               Vous trouverez sur ce portfolio mon Curriculum Vitae, mes compétences, TP, projets...
               </p>
             </div>
@@ -102,58 +102,52 @@
       </div>
     </section>
 
-    <section id="veille">
+    <section id="competences">
+       <h2 class="text-center mb-4">Compétences</h2><br />
       <div class="container">
-        <div class="row">
-          <div class="col-lg-12 text-center">
-            <h2 class="section-heading"><i class="fa fa-1.5x fa-newspaper-o"></i> Veille technologique</h2>
-            <hr class="my-4">
-            <button class="accordion text-center mb-2">Qu'est ce que la veille technologique ? <div class="fa fa-chevron-right fa-1.5x rotate pull-right"></div></button>
-              <div class="panel">
-                <p>La veille technologique ou veille scientifique et technique consiste à s'informer de façon systématique sur les techniques les plus récentes et surtout sur leur mise à disposition commerciale. Aujourd’hui, la veille technologique possède une place majeure <br/>dans le travail d’un informaticien.</p>
-              </div>
+
+        <div id="Pleft">
+          <h5>Pleft</h5>
+          <div class="progress" style="height: 50%;width: 475px;">
+            <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width:80%">
+              80%
+            </div>
           </div>
         </div>
-      </div><br/><br/>
+        <div id="Pright">
+          <h5>Pright</h5>
+          <div class="progress" style="height: 50%;width: 500px;">
+            <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%">
+            40%
+            </div>
+          </div>
+        </div>
+
+   <!--      <div class="progress">
+          <div class="progress-bar progress-bar-striped active" role="progressbar"
+              aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%">40%
+          </div>
+        </div> -->
+      </div>
       <div class="container">
         <div class="row">
-          <div class="col-lg-3 col-md-6 text-center">
-            <a class="liensV service-box mt-5 mx-auto" data-toggle="modal" href="#Veille1">
-              <i class="fa fa-4x fa-heartbeat mb-3 sr-icons"></i>
-              <h3 class="mb-3">Processeur</h3>
-              <p class="text-muted mb-0">Consulter ma veille technologique sur les processeurs.</p>
-            </a>
+          <div class="col-lg-8 mx-auto text-center">
+            <!-- <h2 class="section-heading">Tableau des compétences</h2> -->
+            <br /><hr class="my-4">
+            <p class="mb-5">Compétences relatives aux BTS SIO (Services Informatiques aux Organisations) SLAM (Solutions Logicielles et Applications Métiers)</p>
           </div>
-          <div class="col-lg-3 col-md-6 text-center">
-            <a class="liensV service-box mt-5 mx-auto"  href="/">
-              <i class="fa fa-4x fa-paper-plane mb-3 sr-icons"></i>
-              <h3 class="mb-3">Ready to Ship</h3>
-              <p class="text-muted mb-0">You can use this theme as is, or you can make changes!</p>
-            </a>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 ml-auto text-center">
+              <button class="button button2" type="button" data-toggle="modal" data-target="#compétences"><i class="fa fa-search"></i> Afficher les compétences</button>
           </div>
-          <div class="col-lg-3 col-md-6 text-center">
-            <a class="liensV service-box mt-5 mx-auto"  href="/">
-              <i class="fa fa-4x fa-newspaper-o mb-3 sr-icons"></i>
-              <h3 class="mb-3">Up to Date</h3>
-              <p class="text-muted mb-0">We update dependencies to keep things fresh.</p>
-            </a>
+          <div class="col-lg-4 mr-auto text-center">
+            <p>
+              <a class="button button2 pull-right" href="/doc/CV.pdf" download=""><i class="fa fa-download"></i> Télécharger mon tableau</a>
+            </p>
           </div>
-          <div class="col-lg-3 col-md-6 text-center">
-            <a class="liensV service-box mt-5 mx-auto"  href="/">
-              <i class="fa fa-4x fa-heart mb-3 sr-icons"></i>
-              <h3 class="mb-3">Made with Love</h3>
-              <p class="text-muted mb-0">You have to make your websites with love these days!</p>
-            </a>
-          </div>
-        </div><br/>
-
-
-<hr class="separation"><br/>
-
- <center><p>Liens de mes recherches en rapport avec ma veille technologique</p><iframe src="//www.pearltrees.com/ugop?embed=2&d=201803181548" width="853" height="404" style="border:1px;" allowtransparency="true"></iframe><span style="display:block; padding-top:2px; color:#818181; font-size:13px;"><a href="http://www.pearltrees.com/ugop" style=" color:#818181; font-size:13px;" target="_blank">Ugop</a></span></center>
-
-
-      </div>
+        </div>
+      </div> 
     </section>
 
     <section class="p-0" id="Projets">
@@ -266,48 +260,79 @@
       </div>
     </section>
 
-    <section id="competences">
+    <section id="veille">
       <div class="container">
         <div class="row">
-          <div class="col-lg-8 mx-auto text-center">
-            <h2 class="section-heading">Tableau des compétences</h2>
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading"><i class="fa fa-1.5x fa-newspaper-o"></i> Veille technologique</h2>
             <hr class="my-4">
-            <p class="mb-5">Compétences relatives aux BTS SIO (Services Informatiques aux Organisations) SLAM (Solutions Logicielles et Applications Métiers)</p>
+            <button class="accordion text-center mb-2">Qu'est ce que la veille technologique ? <div class="fa fa-chevron-right fa-1.5x rotate pull-right"></div></button>
+              <div class="panel">
+                <p>La veille technologique ou veille scientifique et technique consiste à s'informer de façon systématique sur les techniques les plus récentes et surtout sur leur mise à disposition commerciale. Aujourd’hui, la veille technologique possède une place majeure <br/>dans le travail d’un informaticien.</p>
+              </div>
           </div>
         </div>
+      </div><br/><br/>
+      <div class="container">
         <div class="row">
-          <div class="col-lg-4 ml-auto text-center">
-              <button class="button button2" type="button" data-toggle="modal" data-target="#myModal"><i class="fa fa-search"></i> Afficher mon tableau</button>
+          <div class="col-lg-3 col-md-6 text-center">
+            <a class="liensV service-box mt-5 mx-auto" data-toggle="modal" href="#Veille1">
+              <i class="fa fa-4x fa-heartbeat mb-3 sr-icons"></i>
+              <h3 class="mb-3">Processeur</h3>
+              <p class="text-muted mb-0">Consulter ma veille technologique sur les processeurs.</p>
+            </a>
           </div>
-          <div class="col-lg-4 mr-auto text-center">
-            <p>
-              <a class="button button2 pull-right" href="/doc/CV.pdf" download=""><i class="fa fa-download"></i> Télécharger mon tableau</a>
-            </p>
+          <div class="col-lg-3 col-md-6 text-center">
+            <a class="liensV service-box mt-5 mx-auto"  href="/">
+              <i class="fa fa-4x fa-paper-plane mb-3 sr-icons"></i>
+              <h3 class="mb-3">Ready to Ship</h3>
+              <p class="text-muted mb-0">You can use this theme as is, or you can make changes!</p>
+            </a>
           </div>
-        </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <a class="liensV service-box mt-5 mx-auto"  href="/">
+              <i class="fa fa-4x fa-newspaper-o mb-3 sr-icons"></i>
+              <h3 class="mb-3">Up to Date</h3>
+              <p class="text-muted mb-0">We update dependencies to keep things fresh.</p>
+            </a>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <a class="liensV service-box mt-5 mx-auto"  href="/">
+              <i class="fa fa-4x fa-heart mb-3 sr-icons"></i>
+              <h3 class="mb-3">Made with Love</h3>
+              <p class="text-muted mb-0">You have to make your websites with love these days!</p>
+            </a>
+          </div>
+        </div><br/>
+
+
+<hr class="separation"><br/>
+
+ <center><p>Liens de mes recherches en rapport avec ma veille technologique</p><iframe src="//www.pearltrees.com/ugop?embed=2&d=201803181548" width="853" height="404" style="border:1px;" allowtransparency="true"></iframe><span style="display:block; padding-top:2px; color:#818181; font-size:13px;"><a href="http://www.pearltrees.com/ugop" style=" color:#818181; font-size:13px;" target="_blank">Ugop</a></span></center>
+
+
       </div>
     </section>
-
    <!--  <hr class="separation"> -->
 
     <section class="bg-primary text-white" id="contact">
       <div class="container">
         <div class="row">
           <div class="col-lg-8 mx-auto text-center">
-            <h2 class="section-heading">Let's Get In Touch!</h2>
+            <h2 class="section-heading">Me contacter</h2>
             <hr class="my-4">
-            <p class="mb-5">Ready to start your next project with us? That's great! Give us a call or send us an email and we will get back to you as soon as possible!</p>
+            <p class="mb-5">N'hésitez pas à me contacter, je répondrais au plus vite !</p>
           </div>
         </div>
         <div class="row">
           <div class="col-lg-4 ml-auto text-center">
             <i class="fa fa-phone fa-3x mb-3 sr-contact"></i>
-            <p>123-456-6789</p>
+            <p style="color: #212529">/</p>
           </div>
           <div class="col-lg-4 mr-auto text-center">
             <i class="fa fa-envelope-o fa-3x mb-3 sr-contact"></i>
             <p>
-              <a href="mailto:your-email@your-domain.com">feedback@startbootstrap.com</a>
+              <a style="color: #212529" href="mailto:your-email@your-domain.com">ugo.perniceni@hotmail.fr</a>
             </p>
           </div>
         </div>
@@ -341,25 +366,101 @@
           <button type="button" class="close" data-dismiss="modal">&times;</button>
         </div>
         <div class="modal-body" style="max-height: 1000px;">
+          <div class="titre-modal"><i class="fa fa-bookmark"></i> Les processeurs</div><br />
+          <p class="description-modal">
+            <ul>
+              <li>
+                Pourquoi les processeurs ?<br /><br/>
+              </li>
+                <i class="fa fa-quote-left fa-pull-left"></i>
+                Ma veille technologique porte sur le thème des processeurs <br/>(ou unité centrale de traitement) car, élément clé d'un ordinateur ils sont représentés <br/>comme le coeur de l'ordinateur.<br/> En constantes évolutions, ils permettent d'exécuter des instructions leurs étant données par le système d’exploitation.<br/><br/>
+              <li>
+                Pour me tenir à jour des avancées technologiques de ce domaines j'utilise :<br/><br/>
+              </li>
+                NetVibes (site permettant de regrouper les flux RSS relatif à un centre d'interêt).
+                Vous pouvez retrouver une capture de mon NetVibes en cliquant sur le bouton suivant,<br/><br/>
+            </ul>
+          </p>
+          <hr><br />
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
 
-  <div class="titre-modal"><i class="fa fa-bookmark"></i> Les processeurs</div><br />
-  <p class="description-modal">
-    <ul>
-      <li>
-        Pourquoi les processeurs ?<br /><br/>
-      </li>
-        <i class="fa fa-quote-left fa-pull-left"></i>
-        Ma veille technologique porte sur le thème des processeurs <br/>(ou unité centrale de traitement) car, élément clé d'un ordinateur ils sont représentés <br/>comme le coeur de l'ordinateur.<br/> En constantes évolutions, ils permettent d'exécuter des instructions leurs étant données par le système d’exploitation.<br/><br/>
-      <li>
-        Pour me tenir à jour des avancées technologiques de ce domaines j'utilise :<br/><br/>
-      </li>
-        NetVibes (site permettant de regrouper les flux RSS relatif à un centre d'interêt).
-        Vous pouvez retrouver une capture de mon NetVibes en cliquant sur le bouton suivant,<br/><br/>
-    </ul>
-  </p>
-  <hr><br />
+  <!-- MODAL myModal -->
+  <div class="modal fade" id="compétences" role="dialog">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title">Compétences relatives au SLAM</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        <div class="modal-body" style="max-height: 1500px;max-width: 1200px;">
+        <p>
+          <ul>
 
+            <li>A1.1.1 – Analyse du cahier des charges d’un service à produire</li>
+            <li>A1.1.2 – Étude de l’impact de l’intégration d’un service sur le système informatique</li>
+            <li>A1.1.3 – Étude des exigences liées à la qualité attendue d’un service</li>
+            <li>A1.2.1 – Élaboration et présentation d’un dossier de choix de solution technique</li>
+            <li>A1.2.2 – Rédaction des spécifications techniques de la solution retenue</li>
+            <li>A1.2.3 – Évaluation des risques liés à l’utilisation d’un service</li>
+            <li>A1.2.4 – Détermination des tests nécessaires à la validation d’un service</li>
+            <li>A1.2.5 – Définition des niveaux d’habilitation associés à un service</li>
+            <li>A1.3.1 – Test d’intégration et d’acceptation d’un service</li>
+            <li>A1.3.2 – Définition des éléments nécessaires à la continuité d’un service</li>
+            <li>A1.3.3 – Accompagnement de la mise en place d’un nouveau service</li>
+            <li>A1.3.4 – Déploiement d’un service</li>
+            <li>A1.4.1 – Participation à un projet</li>
+            <li>A1.4.2 – Évaluation des indicateurs de suivi d’un projet et justification des écarts</li>
+            <li>A1.4.3 – Gestion des ressources</li><br />
+         
+            <li>A2.1.1 – Accompagnement des utilisateurs dans la prise en main d’un service</li>
+            <li>A2.1.2 – Évaluation et maintien de la qualité d’un service</li>
+            <li>A2.2.1 – Suivi et résolution d’incidents</li>
+            <li>A2.2.2 – Suivi et réponse à des demandes d’assistance</li>
+            <li>A2.2.3 – Réponse à une interruption de service</li>
+            <li>A2.3.1 – Identification, qualification et évaluation d’un problème</li>
+            <li>A2.3.2 – Proposition d’amélioration d’un service</li><br />
 
+            <li>A3.1.1 – Proposition d’une solution d’infrastructure</li>
+            <li>A3.1.2 – Maquettage et prototypage d’une solution d’infrastructure</li>
+            <li>A3.2.1 – Installation et configuration d’éléments d’infrastructure</li><br />
+
+            <li>A3.3.2 – Planification des sauvegardes et gestion des restaurations</li><br />
+         
+            <li>A4.1.1 – Proposition d’une solution applicative</li>
+            <li>A4.1.2 – Conception ou adaptation de l’interface utilisateur d’une solution applicative</li>
+            <li>A4.1.3 – Conception ou adaptation d’une base de données</li>
+            <li>A4.1.4 – Définition des caractéristiques d’une solution applicative</li>
+            <li>A4.1.5 – Prototypage de composants logiciels</li>
+            <li>A4.1.6 – Gestion d’environnements de développement et de test</li>
+            <li>A4.1.7 – Développement, utilisation ou adaptation de composants logiciels</li>
+            <li>A4.1.8 – Réalisation des tests nécessaires à la validation d’éléments adaptés ou développés</li>
+            <li>A4.1.9 – Rédaction d’une documentation technique</li>
+            <li>A4.1.10 – Rédaction d’une documentation d’utilisation</li>
+            <li>A4.2.1 – Analyse et correction d’un dysfonctionnement, d’un problème de qualité de service ou de sécurité</li>
+            <li>A4.2.2 – Adaptation d’une solution applicative aux évolutions de ses composants</li>
+            <li>A4.2.3 – Réalisation des tests nécessaires à la mise en production d’éléments mis à jour</li>
+            <li>A4.2.4 – Mise à jour d’une documentation technique</li>
+        ​ 
+            <li>A5.1.1 – Mise en place d’une gestion de configuration</li>
+            <li>A5.1.2 – Recueil d’informations sur une configuration et ses éléments</li>
+            <li>A5.1.3 – Suivi d’une configuration et de ses éléments</li>
+            <li>A5.1.4 – Étude de propositions de contrat de service (client, fournisseur)</li>
+            <li>A5.1.5 – Évaluation d’un élément de configuration ou d’une configuration</li>
+            <li>A5.1.6 – Évaluation d’un investissement informatique</li>
+            <li>A5.2.1 – Exploitation des référentiels, normes et standards adoptés par le prestataire</li>
+            <li>A5.2.2 – Veille technologique</li>
+            <li>A5.2.3 – Repérage des compléments de formation ou d’auto-formation utiles à l’acquisition de nouvelles compétences</li>
+            <li>A5.2.4 – Étude d’une technologie, d’un composant, d’un outil ou d’une méthode</li>
+
+          </ul>
+
+        </p>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
